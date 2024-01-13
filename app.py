c811db67f5ef94a69f519f16b92ae499de7000b6
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-01-13 21:25:22 +0200

    search page, menu page, filter page - DONE, now sign in page and shopping cart

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 import os
 import webbrowser
 
+from jinja2 import Environment
 from flask import render_template, Flask, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = 'lakjfpoek[gf;sldg165478'
 app.config['DEBUG'] = True
+# env = Environment()
+# env.filters['increment'] = lambda value: value + 1
 
 products = [filename for filename in os.listdir('static/images/products')] * 4
+# authors = []
+with open("static/Books_Authors.txt", encoding="utf8") as f:
+    authors = f.readlines()
+with open("static/Books_Dates.txt", encoding="utf8") as f:
+    publishes = f.readlines()
+with open("static/Books_Publishers.txt", encoding="utf8") as f:
+    publishers = f.readlines()
+# with open("static/Books_Others.txt", encoding="utf8") as f:
+#     other = f.readlines()
+    
+    
 
 
 @app.route('/home')
 def home():
-    return render_template('home.html', products=products)
+    return render_template('home.html', products=products, authors=authors, publishes=publishes, publishers=publishers)#, other=other)
 
 
 @app.route('/sign_in')
 def sign_in():
     return render_template('sign_in.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
+
+@app.route('/shopping_cart')
+def shopping_cart():
+    return render_template('shopping_cart.html')
+
+
 
 
 @app.errorhandler(404)
