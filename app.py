f8b075014456cc1ba1ca8737f8fb5f5a5df54b64
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-01-14 23:07:16 +0200

    working on sign in page

--- app.py
+++ app.py
@@ -9,8 +9,16 @@ app.secret_key = 'lakjfpoek[gf;sldg165478'
 app.config['DEBUG'] = True
 # env = Environment()
 # env.filters['increment'] = lambda value: value + 1
+# breadcrumbs = [
+#     {"url": url_for("home"), "text": "الصفحة الرئيسية"},
+#     {"url": url_for(""), "text": "تسجيل الدخول"},
+#     {"url": url_for(""), "text": "عربة التسوق"}
+#     # {"url": url_for("الصفحة", category=category, item=item), "text": item.capitalize()},
+# ]
 
-products = [filename for filename in os.listdir('static/images/products')] * 4
+
+books_lst = [filename for filename in os.listdir('static/images/books')] * 4
+clothes_lst = [filename for filename in os.listdir('static/images/clothes')] * 4
 # authors = []
 with open("static/Books_Authors.txt", encoding="utf8") as f:
     authors = f.readlines()
@@ -21,17 +29,35 @@ with open("static/Books_Publishers.txt", encoding="utf8") as f:
 # with open("static/Books_Others.txt", encoding="utf8") as f:
 #     other = f.readlines()
     
-    
-
 
 @app.route('/home')
 def home():
-    return render_template('home.html', products=products, authors=authors, publishes=publishes, publishers=publishers)#, other=other)
+    return render_template('home.html')
+
+
+@app.route('/books')
+def books():
+    return render_template('books.html', products=books_lst, authors=authors, publishes=publishes, publishers=publishers)#, other=other)
+
+
+@app.route('/clothes')
+def clothes():
+    return render_template('clothes.html', products=clothes_lst)
 
 
 @app.route('/sign_in')
 def sign_in():
     return render_template('sign_in.html')
+
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('forgot_password.html')
 
 
 @app.route('/about')
@@ -63,6 +89,6 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    webbrowser.open("http://127.0.0.1:5000/home")
+    # webbrowser.open("http://127.0.0.1:5000/home")
     app.run()
     
